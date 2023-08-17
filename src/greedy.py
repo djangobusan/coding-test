03cf505b011dc347f61f916273c0c92e29072984
Sangkon Han <djangobusan@gmail.com>
2023-08-17 17:02:52 +0900

    feat: greedy 정답 수정

--- src/greedy.py
+++ src/greedy.py
@@ -55,24 +55,26 @@ def k_knight(p):
     return result
 
 
-# def find_prims(numbers):
-#     from itertools import permutations
-
-#     list_numbers = list(numbers)
-#     result = 0
-#     num = []
-
-#     # 순열조합
-#     for i in range(1, len(list_numbers)+1):
-#         num.append(list(permutations(list_numbers, i)))
-
-#     for i in num:
-#         print(i)
-
-#     # 계산
+def __is_prime__(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0: return False
+    return True
     
-#     # 반환
-#     return result
+def find_prime(numbers):
+    from itertools import permutations
 
-# if __name__ == "__main__":
-#     find_prims(017)
+    list_numbers = list(numbers)
+    result = []
+    num = []
+
+    for i in range(1, len(list_numbers)+1):
+        num.append(list(permutations(list_numbers, i)))
+
+    num = [int("".join(j)) for i in num for j in i]
+
+    for i in num:
+        if __is_prime__(i):
+            result.append(i)
+
+    return len(set(result))
